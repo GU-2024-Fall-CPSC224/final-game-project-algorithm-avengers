e9d8698e1bf7547629adc936ed0eeb259bda004e
KatieP1 <Katiepark1995@yahoo.com>
2025-04-16 22:59:57 -0700

    created beginning intro frame and button

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# CPSC 224 Final Project: [Your project name here]
+# CPSC 224 Final Project: The Ultimate Connect Four
 
 ### Project Description:
+A traditional game of Connect Four, but instead of two players, the game
+will be able to handle up to 4 players, each with their own chosen icon/token
+to represent the disk in the real life game.
 
+Otherwise, the game rules will adhere to the traditional game of Connect Four,
+where a player wins if they connect four of their tokens in 4 successive spots,
+either horizontally, vertically, or diagonally. 
 
 ### Team Information:
 
 - Team Name: Algorithm Avengers  
-- Students on team: Liya 
+- Students on team: McKinely Martsolf, Katie Park, Farhan Moustfa,
+                    Liya Tekie, and Daniel Medina
+
 - Semester: Spring 2025
 
 
--- src/main/java/edu/gonzaga/MainGame.java
+++ src/main/java/edu/gonzaga/MainGame.java
@@ -4,28 +4,81 @@
  * 
  * Project Description:
  * 
+ * A traditional game of Connect Four, but instead of two players, the game
+ * will be able to handle up to 4 players, each with their own chosen icon/token
+ * to represent the disk in the real life game.
  * 
- * Contributors:
+ * Otherwise, the game rules will adhere to the traditional game of Connect Four,
+ * where a player wins if they connect four of their tokens in 4 successive spots,
+ * either horizontally, vertically, or diagonally. 
+ * 
+ * Contributors: McKinely Martsolf, Katie Park, Farhan Moustfa, Liya Tekie, and Daniel Medina
  * 
  * 
- * Copyright: 2023
+ * Copyright: 2025
  */
+
 package edu.gonzaga;
 
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 
 /** Main program class for launching your team's program. */
 public class MainGame {
+
+    static JFrame frame;
+    static JButton playGame;
+    static JFrame game;
+    static JLabel teamIcon;
+
+    static Font myFont = new Font("Roboto", Font.BOLD, 30);
+
     public static void main(String[] args) {
-        System.out.println("this is a test");
+        
+        // creates and configures a frame for the game to load
+        frame = new JFrame("Ultimate Connect Four");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setSize(1000, 1000); 
+        frame.setLayout(null); 
+        frame.setResizable(false);        
 
+        // creates and configures a button that beings the game
+        playGame = new JButton("Play Game");
+        playGame.setBounds(400, 775, 250, 75);
+        playGame.setFont(myFont);
 
+        // imports and configures the background image
+        teamIcon = new JLabel(new ImageIcon("src/main/java/edu/gonzaga/teamIcon.jpeg"));
+        teamIcon.setBounds(0, -28, 1000, 1000);
 
+        // makes it so a new window will pop up if the "play game" button is clicked (will improve later)
+        playGame.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                printWelcomeScreen();
 
-        // Your code here. Good luck!
+            }
+        });
+
+        // adds everything the the actual frame so it will be visible
+        frame.add(playGame);
+        frame.add(teamIcon);
+        frame.setVisible(true);
+
     }
+    
+    static void printWelcomeScreen(){
 
-
-    void printWelcomeScreen(){
+        // new frame is created after "play game" button is clicked
+        // CHANGE THIS LATER
+        game = new JFrame("Figure this out later");
+                    
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        game.setSize(1000, 1000);           
+        game.setLayout(null);              
+        game.setResizable(false);        
+        game.setVisible(true);
 
     }
 
